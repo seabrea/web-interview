cf9e8b87dc9bd9138e2f4a143da1ccd296f21ee0
starbox <arknightsstarbox@protonmail.com>
2023-03-02 16:41:45 +0800

    feat: 节流

--- throttle/index.js
+++ throttle/index.js
@@ -1,26 +1,27 @@
 /**
- * 防抖
+ * 节流
  *
- * 表示在多次触发中只执行最后一次
- * 
- * 常用在点击事件等场景中
+ * 表示在一定时间内中只执行一次
+ *
+ * 常用在滑动监听之类的场景
  */
 
 console.log("start");
 
-const debounce = (func) => {
+const throttle = (func) => {
   let timer = null;
 
   return function () {
-    if (timer) clearTimeout(timer);
+    if (timer) return;
 
     timer = setTimeout(() => {
       func.call(this);
+      timer = null;
     }, 500);
   };
 };
 
-const task = debounce(() => {
+const task = throttle(() => {
   console.log("click");
 });
 
